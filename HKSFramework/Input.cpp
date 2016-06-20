8a7544132b8a5a241124c3e9cdb9dc77f4109f39
yashihei <yashihei@gmail.com>
2016-06-20 21:08:20 +0900

    [add] XInput wrapper

--- HKSFramework/Input.cpp
+++ HKSFramework/Input.cpp
@@ -9,14 +9,19 @@ m_directInput(NULL), m_directInputDevice(NULL)
 	if (FAILED(hr))
 		throw std::runtime_error("Error initialize DirectInput");
 
-	hr = m_directInput->CreateDevice(GUID_SysKeyboard, &m_directInputDevice, NULL);
+	auto hr = m_directInput->CreateDevice(GUID_SysKeyboard, &m_directInputDevice, NULL);
 	if (FAILED(hr))
 		throw std::runtime_error("Error create DirectInput Device");
 
 	//データ形式の設定
 	m_directInputDevice->SetDataFormat(&c_dfDIKeyboard);
+	if (FAILED(hr))
+		throw std::runtime_error("Error setDataFormat");
 	//強調レベルの設定
 	m_directInputDevice->SetCooperativeLevel(hWnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE);
+	if (FAILED(hr))
+		throw std::runtime_error("Error setCooperativeLevel");
+
 	//キーボード入力を有効化
 	m_directInputDevice->Acquire();
 }
@@ -50,4 +55,49 @@ bool Keyboard::isClicked(BYTE code) {
 
 bool Keyboard::isReleased(BYTE code) {
 	return m_state[code][Release];
+}
+
+Mouse::Mouse(HWND hWnd, HINSTANCE hInstance)
+{
+	//FIXME
+	auto hr = DirectInput8Create(hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&m_directInput, NULL);
+	if (FAILED(hr))
+		throw std::runtime_error("Error initialize DirectInput");
+
+	auto hr = m_directInput->CreateDevice(GUID_SysMouse, &m_directInputDevice, NULL);
+	if (FAILED(hr))
+		throw std::runtime_error("Error create DirectInput Device");
+
+	hr = m_directInputDevice->SetDataFormat(&c_dfDIMouse);
+	if (FAILED(hr))
+		throw std::runtime_error("Error setDataFormat");
+
+	hr = m_directInputDevice->SetCooperativeLevel(hWnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE);
+	if (FAILED(hr))
+		throw std::runtime_error("Error setCooperativeLevel");
+}
+
+Mouse::~Mouse() {
+	if (m_directInputDevice)
+		m_directInputDevice->Release();
+	if (m_directInput)
+		m_directInput->Release();
+}
+
+XInput::XInput(int index) :
+m_index(index), m_xInputState{}
+{
+}
+
+void XInput::update() {
+	XInputGetState(m_index, &m_xInputState);
+
+	int buttonIndex = 1;
+	for (int i = 0; i < buttonNum; i++) {
+		m_state[i][Press] = (m_xInputState.Gamepad.wButtons & buttonIndex) != 0;
+		m_state[i][Click] = (m_state[i][Press] ^ m_state[i][Prev]) & m_state[i][Press];
+		m_state[i][Release] = (m_state[i][Press] ^ m_state[i][Prev]) & !m_state[i][Press];
+		m_state[i][Prev] = m_state[i][Press];
+		buttonIndex *= (buttonIndex != 0x0400) ? 2 : 8;
+	}
 }
--- HKSFramework/Input.h
+++ HKSFramework/Input.h
@@ -4,10 +4,7 @@
 #include <bitset>
 #include <array>
 #include <memory>
-
-// MEMO
-// DirectInputによるマウスとキーボードの入力はMSから非推奨とされているので、メッセージループを使うかも？
-// コントローラはとりあえず、XInputのみで
+#include <XInput.h>
 
 class Keyboard {
 public:
@@ -34,17 +31,48 @@ private:
 
 class Mouse {
 public:
-	Mouse(HWND hWnd);
+	Mouse(HWND hWnd, HINSTANCE hInstance);
 	~Mouse();
 private:
 	LPDIRECTINPUT8 m_directInput;
 	LPDIRECTINPUTDEVICE8 m_directInputDevice;
 };
 
-class Input {
+enum class Button {
+	Up,
+	Down,
+	Left,
+	Right,
+	Start,
+	Back,
+	LeftThumb,
+	RightThumb,
+	LeftShoulder,
+	RightShoulder,
+	A, B, X, Y
+};
+
+class XInput {
 public:
-	Input() = default;
+	XInput(int index = 0);
+	~XInput();
+	void update();
+	bool isClicked(Button button);
+	bool isPressed(Button button);
+	bool isReleased(Button button);
+	//Vec2 getLeftThumbDir();
+	//Vec2 getRightThumbDir();
 private:
-	std::shared_ptr<Keyboard> m_keyboard;
-	//マウスとコントローラーもここに
+	int m_index;
+	XINPUT_STATE m_xInputState;
+
+	enum State {
+		Prev,
+		Press,
+		Click,
+		Release,
+		StateNum
+	};
+	static const int buttonNum = 14;
+	std::array<std::bitset<StateNum>, buttonNum> m_state;
 };
